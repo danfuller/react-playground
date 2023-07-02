cbfc1840360760cbb22a5beb0ee21bc9fabee2aa
cococodes <danfuller@users.noreply.github.com>
2023-07-02 13:40:28 +0100

    image and config basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: isGithubActions ? '/react-playground' : '',
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>React Playground</h1>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className={styles.vercelLogo}
+        width={100}
+        height={24}
+        priority
+      />
     </main>
   )
 }
